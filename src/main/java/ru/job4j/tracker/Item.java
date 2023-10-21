576e121e712ab29be86627b0e48bddeff950390a
edgar <edgar_dallakyan@mail.ru>
2023-10-21 17:20:28 +0300

    Перегрузка конструктора [#173890 #454352]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,34 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void printInfo() {
+        System.out.println("id: " + id);
+        System.out.println("name: " + name);
+
+    }
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        item.printInfo();
+        Item one = new Item("Victor");
+        one.printInfo();
+        Item two = new Item(25, "Victor");
+        two.printInfo();
+
+    }
+
     public int getId() {
         return id;
     }
