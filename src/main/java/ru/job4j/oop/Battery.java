1e92ff3a828b1ee683924ccdcdc2286a9cf88534
edgar <edgar_dallakyan@mail.ru>
2023-10-24 10:13:51 +0300

    1.8. Класс использующий сам себя [#167388]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -5,15 +5,17 @@ public class Battery {
     private int load;
 
     public Battery(int load) {
+
         this.load = load;
     }
 
     public String about() {
+
         return "My charge: " + load + "%";
     }
 
     public void exchange(Battery another) {
-        another.load = another.load + this.load;
+        another.load += this.load;
         this.load = 0;
     }
 
