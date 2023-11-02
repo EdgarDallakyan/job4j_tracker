c19a396f8bf85e797663d0a1158413816ce0e740
edgar <edgar_dallakyan@mail.ru>
2023-11-02 14:38:43 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,11 +1,13 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
-    public Item() {
-    }
+    public Item() { }
 
     public Item(String name) {
         this.name = name;
@@ -14,6 +16,10 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public int getId() {
