5164d2dd090a61b1a0752b773c6f32b5b02ae063
edgar <edgar_dallakyan@mail.ru>
2023-11-10 14:56:45 +0300

    6. Метод замены заявки. Tracker.replace [#211748 #458929]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -46,13 +46,12 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        if (index == -1) {
-            return false;
-        } else {
-            items[index].setId(id);
-            items[index].setName(item.getName());
+        if (index != -1) {
+            item.setId(id);
+            items[index] = item;
             return true;
         }
+        return false;
     }
 
     public boolean delete(int id) {
