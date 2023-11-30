fdf74b04d9619362beb1abc72393143907366817
edgar <edgar_dallakyan@mail.ru>
2023-11-30 12:34:10 +0300

    4.2. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void ride() {
+        System.out.println("Автобус едет");
+    }
+
+    @Override
+    public void passanger(int numbers) {
+        System.out.println("Количество пассажиров в автобусе: " + numbers);
+    }
+
+    @Override
+    public double fuel(double litres) {
+        return litres * 62.2;
+    }
+}
