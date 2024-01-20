617509b156b4968c15a99e00daf3d19d98f73c56
edgar <edgar_dallakyan@mail.ru>
2024-01-20 11:11:56 +0300

    0. Map, HashMap [#212652 #474903]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,10 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("pavlov123@mail.ru", "Pavlov Igor Ivanovich");
+        map.put("pogbaturin@yandex.ru", "Debrov Egor Petrovich");
+        map.put("lunatik7169@gmail.com", "Rebrov Artem Sergeevich");
+        map.put("pavlov123@mail.ru", "Paramonov Ivan Victorovich");
+        map.put("leomessi@gmail.com", "Melomed Leonid Andreevich");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " : " + value);
