c4d48ef360eb73415393995274ec9324931906fd
edgar <edgar_dallakyan@mail.ru>
2024-08-28 15:09:40 +0300

    3. Liquibase. Интеграционные тесты. [#504894]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,7 +1,9 @@
 package ru.job4j.tracker;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class Item {
 
@@ -60,5 +62,23 @@ public class Item {
                 + ", name='" + name + '\''
                 + ", created=" + created.format(FORMATTER)
                 + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return Objects.equals(name, item.name)
+                && (created.format(FORMATTER).equals(item.created.format(FORMATTER)));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
