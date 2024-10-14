a3299b0833b82d38bdb7411fb641b93de44f040d
edgar <edgar_dallakyan@mail.ru>
2024-10-14 16:56:11 +0300

    1. Профилирование. [#1591]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -38,7 +38,7 @@ public class StartUI {
         Input input = new ValidateInput(
                 output, new ConsoleInput()
         );
-        try (Store tracker = new SqlTracker()) {
+        try (Store tracker = new MemTracker()) {
             List<UserAction> actions = List.of(
                     new Create(output),
                     new Replace(output),
@@ -46,7 +46,9 @@ public class StartUI {
                     new FindAll(output),
                     new FindById(output),
                     new FindByName(output),
-                    new Exit(output)
+                    new Exit(output),
+                    new CreateManyItems(output),
+                    new DeleteAllItems(output)
             );
             new StartUI(output).init(input, tracker, actions);
         } catch (Exception e) {
